d8b6739c36b9ad57b4e5fdc64ef9db74aef3594a
Phani-TrainingPrograms <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2026-01-29 16:54:42 +0530

    Updated with Day 3 Contents
    Covered JS Programming constructs

---
+++ HTML/Javascript/mathCalc.js
@@ -0,0 +1,7 @@
+//A file to have only JS based Functions. No DOM related Functions shall be called here. 
+const addFunc = (v1, v2) => v1 + v2
+const subFunc = (v1, v2) => v1 - v2;
+const mulFunc = (v1, v2) => v1 * v2;
+const divFunc = (v1, v2) => v1 / v2;
+const sqrtFunc = (v1) => Math.sqrt(v1);
+const sqrFunc = (v1) => mulFunc(v1, v2);
