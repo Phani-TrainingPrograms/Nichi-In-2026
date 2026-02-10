b3e526935022e741a6ea826bf816e5635e3ac05c
Phani-TrainingPrograms <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2026-02-10 16:49:40 +0530

    Implement POST data handling in server
    Added POST request handling to process incoming data from clients.

--- Nodejs/Ex10PageHandling.js
+++ Nodejs/Ex10PageHandling.js
@@ -10,11 +10,26 @@ function renderPage(fileName, res){
     fs.createReadStream(file).pipe(res);//read the file and pipe it to the response object, which will send it to the client.pipe method is used to read the stream and send t to the response object. It is more efficient than reading the entire file into memory and then sending it to the client. 
 }
 
+//When data is posted to the server, the data shall be available from the body section of the req. We shall handle the data event of the req object and read the data from the body of the req object. 
+function handlePostData(req, res){
+    let body = "";
+    req.on("data", (chunk) => {
+        body += chunk;
+    });
+    req.on("end", () => {
+        console.log("Data received from client: " + body);
+        res.end("Data received from client: " + body);
+    });
+}
+
 function processRequest(req, res){
     if(req.url === "/favicon.ico"){
         return;
     }
-    console.log(req.url)
+    if(req.method == 'POST'){
+        handlePostData(req, res);
+    }
+
     if(req.url === "/"){
         res.end("<h1>Welcome to Phaniraj Training</h1>")
     }else if(req.url === "/reg"){
